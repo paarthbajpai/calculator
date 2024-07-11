2c757eb0ee280c08b17173bba213c8aed2847054
paarthbajpai <paarthbajpai16@gmail.com>
2024-07-11 16:30:13 +0530

    js file

---
+++ script.js
@@ -0,0 +1,30 @@
+let display = document.getElementById('display');
+
+function clearDisplay() {
+    display.innerText = '0';
+}
+
+function deleteLast() {
+    if (display.innerText.length > 1) {
+        display.innerText = display.innerText.slice(0, -1);
+    } else {
+        display.innerText = '0';
+    }
+}
+
+function appendCharacter(char) {
+    if (display.innerText === '0') {
+        display.innerText = char;
+    } else {
+        display.innerText += char;
+    }
+}
+
+function calculateResult() {
+    try {
+        display.innerText = eval(display.innerText);
+    } catch {
+        display.innerText = 'Error';
+    }
+}
+
